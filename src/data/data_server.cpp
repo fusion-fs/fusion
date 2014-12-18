03a6098a2f07cc9c5cf26eac37aa8930a8b31302
Huamin Chen <fusion.filesystem@gmail.com>
2014-12-18 10:20:58 -0500

    improve data server structure
    Signed-off-by: Huamin Chen <fusion.filesystem@gmail.com>

--- src/data/data_server.cpp
+++ src/data/data_server.cpp
@@ -1,6 +1,5 @@
 #include "io.pb.h"
 #include "data_server.hpp"
-#include "packedmessage.hpp"
 #include <cassert>
 #include <iostream>
 #include <map>
@@ -17,17 +16,9 @@ using namespace std;
 namespace asio = boost::asio;
 using asio::ip::tcp;
 using boost::uint8_t;
+using namespace io;
 
 
-#define DEBUG true
-
-
-typedef map<string, string> StringDatabase;
-
-
-// Database connection - handles a connection with a single client.
-// Create only through the Session::create factory.
-//
 class Session : public boost::enable_shared_from_this<Session>
 {
 public:
@@ -35,9 +26,9 @@ public:
     typedef boost::shared_ptr<io::Request> RequestPointer;
     typedef boost::shared_ptr<io::Response> ResponsePointer;
 
-    static Pointer create(asio::io_service& io_service, StringDatabase& db)
+    static Pointer create(asio::io_service& io_service)
     {
-        return Pointer(new Session(io_service, db));
+        return Pointer(new Session(io_service));
     }
 
     tcp::socket& get_socket()
@@ -52,59 +43,41 @@ public:
 
 private:
     tcp::socket m_socket;
-    StringDatabase& m_db_ref;
     vector<uint8_t> m_readbuf;
-    PackedMessage<io::Request> m_packed_request;
+    boost::shared_ptr<io::Request> m_request;
 
-    Session(asio::io_service& io_service, StringDatabase& db)
-        : m_socket(io_service), m_db_ref(db),
-        m_packed_request(boost::shared_ptr<io::Request>(new io::Request()))
+    Session(asio::io_service& io_service)
+        : m_socket(io_service), m_request(new io::Request())
     {
     }
     
     void handle_read_header(const boost::system::error_code& error)
     {
-        DEBUG && (cerr << "handle read " << error.message() << '\n');
         if (!error) {
-            DEBUG && (cerr << "Got header!\n");
-            DEBUG && (cerr << show_hex(m_readbuf) << endl);
-            unsigned msg_len = m_packed_request.decode_header(m_readbuf);
-            DEBUG && (cerr << msg_len << " bytes\n");
+            unsigned msg_len;
             start_read_body(msg_len);
         }
     }
 
     void handle_read_body(const boost::system::error_code& error)
     {
-        DEBUG && (cerr << "handle body " << error << '\n');
         if (!error) {
-            DEBUG && (cerr << "Got body!\n");
-            DEBUG && (cerr << show_hex(m_readbuf) << endl);
             handle_request();
             start_read_header();
         }
     }
 
-    // Called when enough data was read into m_readbuf for a complete request
-    // message. 
-    // Parse the request, execute it and send back a response.
-    //
     void handle_request()
     {
-        if (m_packed_request.unpack(m_readbuf)) {
-            RequestPointer req = m_packed_request.get_msg();
-            ResponsePointer resp = prepare_response(req);
-            
-            vector<uint8_t> writebuf;
-            PackedMessage<io::Response> resp_msg(resp);
-            resp_msg.pack(writebuf);
-            asio::write(m_socket, asio::buffer(writebuf));
-        }
+        RequestPointer req;
+        ResponsePointer resp = prepare_response(req);
+        
+        vector<uint8_t> writebuf;
+        asio::write(m_socket, asio::buffer(writebuf));
     }
 
     void start_read_header()
     {
-        m_readbuf.resize(HEADER_SIZE);
         asio::async_read(m_socket, asio::buffer(m_readbuf),
                 boost::bind(&Session::handle_read_header, shared_from_this(),
                     asio::placeholders::error));
@@ -116,8 +89,8 @@ private:
         // bytes. Expand it to fit in the body as well, and start async
         // read into the body.
         //
-        m_readbuf.resize(HEADER_SIZE + msg_len);
-        asio::mutable_buffers_1 buf = asio::buffer(&m_readbuf[HEADER_SIZE], msg_len);
+        m_readbuf.resize(msg_len);
+        asio::mutable_buffers_1 buf = asio::buffer(m_readbuf, msg_len);
         asio::async_read(m_socket, buf,
                 boost::bind(&Session::handle_read_body, shared_from_this(),
                     asio::placeholders::error));
@@ -152,7 +125,6 @@ struct DataServer::DataServerImpl
 {
     asio::io_service& io_service_;
     tcp::acceptor acceptor;
-    StringDatabase db;
 
     DataServerImpl(asio::io_service& io_service, unsigned port)
         : io_service_(io_service), 
@@ -169,7 +141,7 @@ struct DataServer::DataServerImpl
         // single-threaded.
         //
         Session::Pointer new_connection = 
-            Session::create(io_service_, db);
+            Session::create(io_service_);
 
         // Asynchronously wait to accept a new client
         //
